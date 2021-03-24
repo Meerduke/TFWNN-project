add40afea2de0d2d2fbb3c99840867cecb2a2208
Meerduke <Meerduke@users.noreply.github.com>
2021-03-24 17:45:21 +0100

    index.html link added

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const email = document.getElementById("mail");
-
-email.addEventListener("input", function (event) {
-    if (email.validity.valueMissing) {
-        email.setCustomValidity("Please enter your email to attend the screening");
-    } else {
-        email.setCustomValidity("");
-    }
-});
